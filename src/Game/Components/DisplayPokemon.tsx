4d945cc54b7c2479ec710b71832c7e55ae3d2143
jayshah2005 <jayshah3616@gmail.com>
2025-12-24 18:36:58 -0500

    fix cannot click a card  after game ends

--- src/Game/Components/DisplayPokemon.tsx
+++ src/Game/Components/DisplayPokemon.tsx
@@ -1,8 +1,13 @@
 import { playCry } from "../api/pokeapi";
 import Tilt from "react-parallax-tilt";
 import { GameStaus } from "../../global";
+import { useState } from "react";
 
 export function DisplayPokemon({ pokemon, setPokemon, selectedPokemon, setSelectedPokemon, gameStatus, setGameStatus }: { pokemon: any; setPokemon : any, selectedPokemon: any; setSelectedPokemon: any, gameStatus: any; setGameStatus: any }) {
+
+  // let numberOfPokemonToDisplay = 8;
+  // let unSelectedPokemon = [...pokemon];
+  // let [displayPokemon, setDisplayPokemon] = useState([...pokemon.slice(0, numberOfPokemonToDisplay)]);
 
   if (selectedPokemon.length === pokemon.length) {
       setGameStatus(GameStaus.WON);
@@ -39,18 +44,39 @@ export function DisplayPokemon({ pokemon, setPokemon, selectedPokemon, setSelect
 
   function handlePokemonClick(event: React.MouseEvent, currPokemon: any) {
 
-    playCry(currPokemon.cryUrl);
+    if(gameStatus !== GameStaus.NOT_DETERMINED) return;
 
-    if(selectedPokemon.includes(currPokemon.id)){
+    if(selectedPokemon.includes(currPokemon)){
       setGameStatus(GameStaus.LOST);
     }
 
-    setSelectedPokemon([...selectedPokemon, currPokemon.id]);
+    setSelectedPokemon([...selectedPokemon, currPokemon]);
+
+    playCry(currPokemon.cryUrl);
+
     setPokemon([...shuffle(pokemon)]);
+
+    // unSelectedPokemon = pokemon.filter((p) => p.id !== currPokemon.id)
+    // let newSelectedPokemon = [...selectedPokemon, currPokemon];
+
+    // setSelectedPokemon(newSelectedPokemon);
+
+    // let newPokemon = [...newSelectedPokemon];
+
+
+    // if (newPokemon.length >= numberOfPokemonToDisplay) {
+    //   newPokemon = newPokemon.slice(0, numberOfPokemonToDisplay - 2);
+    //   let rand = Math.floor(Math.random() * unSelectedPokemon.length);
+    //   newPokemon.push(unSelectedPokemon[rand], unSelectedPokemon[rand + 1]);
+    // }
+
+    // newPokemon.push(...unSelectedPokemon.splice(0, numberOfPokemonToDisplay - newPokemon.length));
+    
+    // setDisplayPokemon([...shuffle(Array.from(newPokemon))]);
   }
 
   function shuffle(array) {
-    
+
     // Iterate over the array in reverse order
     for (let i = array.length - 1; i > 0; i--) {
       // Generate Random Index
@@ -58,8 +84,9 @@ export function DisplayPokemon({ pokemon, setPokemon, selectedPokemon, setSelect
 
       // Swap elements
       [array[i], array[j]] = [array[j], array[i]];
-      }
+    }
+
+    console.log(array)
 	  return array;
   }
-
 }
