5351042c2d9ae3ba6b06118504511d87cc452311
jayshah2005 <jayshah3616@gmail.com>
2026-01-04 22:05:31 -0500

    fix seperated play/pause logic from reusable components

--- src/Game/Components/MusicButtons.tsx
+++ src/Game/Components/MusicButtons.tsx
@@ -13,15 +13,8 @@ export function toggleAudio(audioRef: React.RefObject<HTMLAudioElement>) {
 }
 
 export function pauseAudio(audioRef: React.RefObject<HTMLAudioElement>) {
-    const handlePause = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
-        e.stopPropagation();
-        if (audioRef.current) {
-            audioRef.current.pause();
-        }
-    };
-
     return (
-        <svg className="button" viewBox="0 0 60 60" onClick={handlePause}>
+        <svg className="button" viewBox="0 0 60 60" onClick={(e) => handlePause(e, audioRef)}>
             <rect x="0" y="0" width="20" height="60" />
             <rect x="30" y="0" width="20" height="60" />
         </svg>
@@ -29,16 +22,23 @@ export function pauseAudio(audioRef: React.RefObject<HTMLAudioElement>) {
 }
 
 export function playAudio(audioRef: React.RefObject<HTMLAudioElement>) {
-    const handlePlay = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+    return (
+        <svg className="button" viewBox="0 0 60 60" onClick={(e) => handlePlay(e, audioRef)}>
+            <polygon points="0,0 50,30 0,60" />
+        </svg>
+    );
+}
+
+export function handlePause(e: React.MouseEvent<SVGSVGElement, MouseEvent>, audioRef: React.RefObject<HTMLAudioElement>) {
+        e.stopPropagation();
+        if (audioRef.current) {
+            audioRef.current.pause();
+        }
+};
+
+export function handlePlay(e: React.MouseEvent<SVGSVGElement, MouseEvent>, audioRef: React.RefObject<HTMLAudioElement>) {
         e.stopPropagation();
         if (audioRef.current) {
             audioRef.current.play();
         }
-    };
-
-    return (
-        <svg className="button" viewBox="0 0 60 60" onClick={handlePlay}>
-            <polygon points="0,0 50,30 0,60" />
-        </svg>
-    );
 }
